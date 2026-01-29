199585ab9af12afc617b1587e9a335d0861205c9
Merna Ayman Shawky <58171140+mernaayman99@users.noreply.github.com>
2026-01-29 02:38:27 +0200

    Add files via upload

---
+++ module-1-spark-ui-analysis/Demo Introduction Video.py
@@ -0,0 +1,72 @@
+# Databricks notebook source
+# MAGIC %sql
+# MAGIC SHOW SCHEMAS IN databricks_simulated_retail_customer_data;
+
+# COMMAND ----------
+
+# MAGIC %sql
+# MAGIC SHOW TABLES IN databricks_simulated_retail_customer_data.v01;
+
+# COMMAND ----------
+
+df = spark.table(
+    "databricks_simulated_retail_customer_data.v01.sales_orders"
+)
+
+df.printSchema()
+
+# COMMAND ----------
+
+df.select("customer_id", "order_number")
+
+# COMMAND ----------
+
+df.count()
+
+# COMMAND ----------
+
+customer_counts = (
+    df.groupBy("customer_id")
+      .count()
+      .orderBy("count", ascending=False)
+)
+
+customer_counts.show()
+
+# COMMAND ----------
+
+spark.conf.set("spark.sql.shuffle.partitions", 200)
+
+heavy_shuffle = (
+    df.select("customer_id", "order_number")
+      .groupBy("customer_id")
+      .count()
+      .orderBy("count", ascending=False)
+)
+
+heavy_shuffle.collect()
+
+# COMMAND ----------
+
+skew_demo = (
+    df.groupBy("number_of_line_items")
+      .count()
+      .orderBy("count", ascending=False)
+)
+
+skew_demo.show()
+
+# COMMAND ----------
+
+df.groupBy("customer_id").count().collect()
+
+# COMMAND ----------
+
+optimized = (
+    df.repartition(50, "customer_id")
+      .groupBy("customer_id")
+      .count()
+      .orderBy("count", ascending=False)
+)
+
+optimized.collect()
